c765c728e076e79b2a00b5c6cc7c29a3693acdd9
HubertDe <dekutowski@outlook.com>
2025-05-26 22:19:31 +0200

    Added a board

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
-// Necessary header files for input output functions
 #include <iostream>
+#include <vector>
 
-// main() function: where the execution of
-// C++ program begins
+using std::cout;
+using std::vector;
+using std::string;
+using std::endl;
+
+class Board {
+	private:
+		int frow;
+		int fcolumn;
+		vector<vector<string>> fboard {{"%","a","b","c","d","e","f","g","h"},
+										{"8","o","x","o","x","o","x","o","x"},
+										{"7","x","o","x","o","x","o","x","o"},
+										{"6","o","x","o","x","o","x","o","x"},
+										{"5","x","o","x","o","x","o","x","o"},
+										{"4","o","x","o","x","o","x","o","x"},
+										{"3","x","o","x","o","x","o","x","o"},
+										{"2","o","x","o","x","o","x","o","x"},
+										{"1","x","o","x","o","x","o","x","o"},};							
+	public:
+		Board() {
+			for(frow = 0; frow < 9; frow ++){
+					cout<<endl;
+				for (fcolumn = 0 ;fcolumn < 9; fcolumn++){
+					cout<< fboard[frow][fcolumn];
+				}
+		}
+	}
+};
+
+
+
 int main() {
-  
-    // This statement prints "Hello World"
-	std::cout << "Hello World";
+    Board board;
 
     return 0;
 }
