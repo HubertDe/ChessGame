8d142eb0305414f564e90b6948e593c21e7ee5d5
HubertDe <dekutowski@outlook.com>
2025-05-31 17:10:45 +0200

    Added posiotions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using std::cout;
 using std::vector;
 using std::string;
 using std::endl;
+using std::map;
 
 class Board {
 	private:
@@ -18,7 +20,17 @@ class Board {
 										{"4","o","x","o","x","o","x","o","x"},
 										{"3","x","o","x","o","x","o","x","o"},
 										{"2","o","x","o","x","o","x","o","x"},
-										{"1","x","o","x","o","x","o","x","o"},};							
+										{"1","x","o","x","o","x","o","x","o"},};						
+		map<string, vector<int>> positions = {
+			{"a8",{1,1}},{"b8",{1,2}},{"c8",{1,3}},{"d8",{1,4}},{"e8",{1,5}},{"f8",{1,6}},{"g8",{1,7}},{"h8",{1,8}},
+			{"a7",{2,1}},{"b7",{2,2}},{"c7",{2,3}},{"d7",{2,4}},{"e7",{2,5}},{"f7",{2,6}},{"g7",{2,7}},{"h7",{2,8}},
+			{"a6",{3,1}},{"b6",{3,2}},{"c6",{3,3}},{"d6",{3,4}},{"e6",{3,5}},{"f6",{3,6}},{"g6",{3,7}},{"h6",{3,8}},
+			{"a5",{4,1}},{"b5",{4,2}},{"c5",{4,3}},{"d5",{4,4}},{"e5",{4,5}},{"f5",{4,6}},{"g5",{4,7}},{"h5",{4,8}},
+			{"a4",{5,1}},{"b4",{5,2}},{"c4",{5,3}},{"d4",{5,4}},{"e4",{5,5}},{"f4",{5,6}},{"g4",{5,7}},{"h4",{5,8}},
+			{"a3",{6,1}},{"b3",{6,2}},{"c3",{6,3}},{"d3",{6,4}},{"e3",{6,5}},{"f3",{6,6}},{"g3",{6,7}},{"h3",{6,8}},
+			{"a2",{7,1}},{"b2",{7,2}},{"c2",{7,3}},{"d2",{7,4}},{"e2",{7,5}},{"f2",{7,6}},{"g2",{7,7}},{"h2",{7,8}},
+			{"a1",{8,1}},{"b1",{8,2}},{"c1",{8,3}},{"d1",{8,4}},{"e1",{8,5}},{"f1",{8,6}},{"g1",{8,7}},{"h1",{8,8}},
+		};
 	public:
 		Board() {
 			for(row = 0; row < 9; row ++){
@@ -164,6 +176,26 @@ class WhitePawn: private Pawn{
 				image = "/u2659";
 			}
 };
+
+/*class Game{
+	private:
+		int turn;
+		Board Board;
+		WhiteKing WhiteKing;
+		BlackKing BlackKing
+		WhiteQueen WhiteQueen;
+		BlackQueen BlackQueen;
+		WhiteRook WhiteRook;
+		BlackRook BlackRook;
+		WhiteBishop WhiteBishop;
+		BlackBishop BlackBishop;
+		WhiteKnight WhiteKnight;
+		BlackKnight BlackKnight;
+		WhitePawn WhitePawn;
+		BlackPawn BlackPawn;
+
+	public:
+};*/
 
 int main() {
     Board board;
